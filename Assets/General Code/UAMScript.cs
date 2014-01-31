c9cabec91a0bb63260ba2f24a739adb373d8bd7b
Xander Flood <xanderflood@Xanders-MacBook-Pro.local>
2014-01-30 20:10:38 -0500

    Graphics for unit action menu

---
+++ Assets/General Code/UAMScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UAMScript : MonoBehaviour {
+
+	public Unit target;
+	public bool onFire;
+
+	public float diff;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.FindChild ("MenuIcon").transform.position;
+
+		if (Input.GetKeyDown("up") && !onFire) {
+			onFire = true;
+			pos.y += diff;
+		}
+
+		if (Input.GetKeyDown("down") && onFire) {
+			onFire = false;
+			pos.y -= diff;
+		}
+
+		if (Input.GetKeyDown ("space")) {
+			if (onFire) {
+				//do attack
+			} else {
+				//cancel
+			}
+		}
+
+		transform.FindChild ("MenuIcon").transform.position = pos;
+	}
+}
